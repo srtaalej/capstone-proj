e66b51356e0a2abef6ea2ebe56b7da904362b3e5
Tony Wu <tonywu@Tonys-MacBook-Pro.local>
2025-02-21 21:08:33 -0500

    tested deployment onto chain, deployed program id: CqQWuZSz2waA7QK9DoF6bUFcdmLhuw2oveDqrLP9gzRv

--- blockchain_ware/nft_id_contract/programs/nft_id_contract/src/lib.rs
+++ blockchain_ware/nft_id_contract/programs/nft_id_contract/src/lib.rs
@@ -1,13 +1,13 @@
 use anchor_lang::prelude::*;
 
-declare_id!("F2KMtCzRhtG5kvc9R4Av37GCrhGzjDLbrE9TwEJcdozm");
+declare_id!("CqQWuZSz2waA7QK9DoF6bUFcdmLhuw2oveDqrLP9gzRv");
 
 #[program]
 pub mod nft_id_contract {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        msg!("Hello there from: {:?}", ctx.program_id);
         Ok(())
     }
 }
