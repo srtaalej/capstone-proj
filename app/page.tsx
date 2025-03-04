36b768d4a556d15305703648291a811f00393155
Maria Alejandra <104795114+srtaalej@users.noreply.github.com>
2025-03-04 13:42:00 -0500

    feat: new navbar with create poll button and authentication flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,13 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
-import { useWallet } from "@solana/wallet-adapter-react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import Navbar from "./components/landing_page/user_navbar_card";
+
 
 export default function Home() {
-  const { publicKey, disconnect } = useWallet();
-
   return (
     <div className="min-h-screen bg-gray-100">
-      <nav className="flex justify-between items-center p-4 bg-white shadow-lg">
-        <div className="text-xl font-bold text-gray-800">Block Vote</div>
-        <div>
-          <Link href="/faq" className="text-black hover:underline">
-            FAQ
-          </Link>
-        </div>
-      </nav>
+      <Navbar/>
       <main className="flex flex-col items-center justify-center flex-1 p-6">
         <h1 className="text-5xl font-semibold text-gray-800 my-8 text-center">
           Block Vote
@@ -25,18 +15,6 @@ export default function Home() {
         <p className="text-xl text-gray-600 mb-6 text-center">
           A decentralized voting system using blockchain technology.
         </p>
-        <WalletMultiButton>{publicKey ? "Authenticated" : "Authenticate"}</WalletMultiButton>
-        {publicKey && (
-          <p className="mt-4 text-gray-700">Connected: {publicKey.toString()}</p>
-        )}
-        {publicKey && (
-          <button
-            onClick={disconnect}
-            className="mt-4 px-6 py-3 bg-red-600 text-white font-medium rounded-lg shadow-md hover:bg-red-700 transition"
-          >
-            Logout
-          </button>
-        )}
       </main>
     </div>
   );
