a87c3268b1bd114d3de1fceb04b2949a40086034
Maria Alejandra <104795114+srtaalej@users.noreply.github.com>
2025-04-08 15:33:21 -0400

    vote modal for public polls

--- app/public_polls/page.tsx
+++ app/public_polls/page.tsx
@@ -5,14 +5,19 @@ import PollCard from '../components/polls/poll_card';
 import { useDebounce } from 'use-debounce';
 import { createClient } from '@/app/lib/client';
 import { Poll } from '@/app/types/poll';
+import VoteModal from '../components/polls/vote_modal';
+import { useWallet } from '@solana/wallet-adapter-react';
 
 const PublicPollsPage = () => {
+    const connected  = useWallet();
     const [polls, setPolls] = useState<any[]>([]);
     const supabase = createClient();
     const [searchQuery, setSearchQuery] = useState('');
     const [debouncedQuery] = useDebounce(searchQuery, 300);
     const [searchResults, setSearchResults] = useState<Poll[]>([]);
     const [isSearching, setIsSearching] = useState(false);
+    const [selectedPoll, setSelectedPoll] = useState<Poll | null>(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     // Fetch all public polls (is_private = false)
     useEffect(() => {
@@ -49,20 +54,40 @@ const PublicPollsPage = () => {
       fetchPolls();
     }, []);
 
+    // Handle poll selection
+    const handlePollClick = (poll: Poll) => {
+    if (!connected) {
+      setIsModalOpen(false);
+    } else {
+      setSelectedPoll(poll);
+      setIsModalOpen(true);
+    }
+    };
+
+    // Close modal
+    const handleCloseModal = () => {
+      setIsModalOpen(false);
+      setSelectedPoll(null);
+    };
+
     return (
       <div className="space-y-6 p-10 bg-gray-900">
-      <h2 className="text-2xl font-semibold text-white">Public Polls</h2>
-      <ul role="list" className="grid grid-cols-2 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            
-              {polls.map((poll: Poll) => (
-                  <PollCard
-                      key={poll.id}
-                      poll={poll}
-                  />
-              ))}
-           
-          </ul>
-  </div>
+        <h2 className="text-2xl font-semibold text-white">Public Polls</h2>
+        <ul role="list" className="grid grid-cols-2 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {selectedPoll && (
+              <VoteModal
+                poll={selectedPoll}
+                isOpen={isModalOpen}
+                onClose={handleCloseModal}
+              />
+            )}
+          {polls.map((poll: Poll) => (
+            <li key={poll.id} onClick={() => handlePollClick(poll)} className="cursor-pointer">
+              <PollCard poll={poll} />
+            </li>
+          ))}
+        </ul>
+      </div>
     );
 };
 
